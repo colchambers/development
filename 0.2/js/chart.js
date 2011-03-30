f1d8515b1e232c4112327c06f3aa0f3f5d847a0f
colchambers <colchambers@gmail.com>
2011-03-30 14:56:26 +0000

    refactored chart into an object. removed word chart from method names. Updated all references. Moved visualisation type definition to chart. Makes more sense there.

--- 0.2/js/chart.js
+++ 0.2/js/chart.js
@@ -1,35 +1,47 @@
 /*
  * refactored from embeddable chart. Defines basic chart functions. Needs refactoring to a class. 
  */
-
-/*
- * Create a default chartOptions object. Draw a chart using chartOptions and data objects
- * @return string
- */
-function initialiseChart() {
-    drawVisualization(visualisationType);
+function Visualisation_Chart(){
+    
+    var chart = null;
+    
+    /*
+     * Create a default chartOptions object. Draw a chart using chartOptions and data objects
+     * @return string
+     */
+    this.initialise = initialise;
+    function initialise() {
+        this.draw(visualisationType);
+    }
+    
+    this.createChart = createChart;
+    function createChart(elementId, typeId){
+        return eval('new google.visualization.'+chartTypes[parseInt(typeId)].method+'(document.getElementById(elementId))');
+    }
+    
+    this.draw = draw;
+    function draw(typeId) {
+    
+        if(!chart){
+            // Create and draw the visualization.
+            chart = this.createChart(chartElementId, typeId); //new google.visualization.PieChart(document.getElementById('chart_div'))
+        }
+        chart.draw(data, chartOptions);
+    }
+    
+    /*
+     * create a set of default chart options. 
+     * @return object
+     */
+    this.createDefaultOptions = createDefaultOptions;
+    function createDefaultOptions(){
+        return {width: defaultChartWidth, height: defaultChartHeight, is3D: defaultChartIs3D};
+    }
 }
 
-function createChart(elementId, typeId){
-    return eval('new google.visualization.'+chartTypes[parseInt(typeId)].method+'(document.getElementById(elementId))');
-}
-
-function drawVisualization(typeId) {
-
-    // Create and draw the visualization.
-    chart = createChart(chartElementId, typeId); //new google.visualization.PieChart(document.getElementById('chart_div'))
-  
-    chart.draw(data, chartOptions);
-}
-
-function updateVisualization(typeId) {
-    chart.draw(data, chartOptions);
-}
-      
-/*
- * create a set of default chart options. 
- * @return object
- */
-function createDefaultChartOptions(){
-    return {width: defaultChartWidth, height: defaultChartHeight, is3D: defaultChartIs3D};
+function VisualisationType (type, title, method){
+    
+    this.type = type;
+    this.title = title;
+    this.method = method
 }
