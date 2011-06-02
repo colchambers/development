a6f2f94d8331a53fd5144b2ab28e98cfa48deea7
colchambers <colchambers@gmail.com>
2011-06-02 10:18:23 +0000

    updated analyse.js to standard Out class. Adjusted metabolic rate to fit

--- 0.2/js/analyse.js
+++ 0.2/js/analyse.js
@@ -1,26 +1,56 @@
- function initialiseConsole(){
- // Create console
-    // Create a YUI instance and request the console module and its dependencies
-    YUI({ filter: 'raw' }).use("console", "console-filters", "dd-plugin", function (Y) {
-     
-        console = new Y.Console({
-    		strings: {
-                logSource: Y.Global,
-    			title : 'Draggable Console with filters!',
-    			pause : 'Wait',
-    			clear : 'Flush',
-    			collapse : 'Shrink',
-    			expand : 'Grow'
-    		}
-    	}).plug(Y.Plugin.ConsoleFilters)
-    	  .plug(Y.Plugin.Drag, { handles: ['.yui3-console-hd'] })
-    	  .render();
-    	 
-    });
- }
  
- function debug(message, filter){
-    YUI({ filter: 'raw' }).use("node", function (Y) {
-            Y.log(message, filter);
-    });
+ var Out = {
+    initialiseConsole: function (){
+     // Create console
+       /* // Create a YUI instance and request the console module and its dependencies
+        YUI({ filter: 'raw' }).use("console", "console-filters", "dd-plugin", function (Y) {
+         
+            console = new Y.Console({
+                strings: {
+                    logSource: Y.Global,
+        			title : 'Draggable Console with filters!',
+        			pause : 'Wait',
+        			clear : 'Flush',
+        			collapse : 'Shrink',
+        			expand : 'Grow'
+        		}
+        	}).plug(Y.Plugin.ConsoleFilters)
+        	  .plug(Y.Plugin.Drag, { handles: ['.yui3-console-hd'] })
+        	  .render();
+        	 
+        });
+        */
+    },
+    append: function (message, filter){
+        YUI({ filter: 'raw' }).use("node", function (Y) {
+                Y.log(message, filter);
+        });
+    
+    },
+     iterate: function (variable, message, full){
+         message == message?message: 'Variable = ';
+             
+         if(variable.length){
+             for( x=0;x<variable.length;x++ ){
+            	this.append( message + " ( " + x + " ) = " + variable[ x ] )
+    		}
+         }
+         
+         var method;
+         for( method in variable ){
+    		if( full ){
+				this.append( method + " = " + this.getObjectMethod( variable, method ));
+			}
+			else{
+				this.append( method);
+			}
+		}
+     },
+     object: function (variable, message, full){
+         message == message?message: 'Variable = ';
+         this.append(message);
+         this.append(variable);
+     },
+     getObjectMethod: function ( variable, method ){ return variable[ method ]; }
+    
 }
