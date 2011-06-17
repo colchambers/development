b5be8e408fcbf8064b3d913796edaf624aeb7507
Colin Chambers <c.chambers@open.ac.uk>
2011-06-17 12:59:21 +0100

    added index file

---
+++ 0.2/index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+<html>
+<body>
+	Welcome to localhost/metabolise/v0.2
+	
+	Please choose from the following options. 
+	<ul>
+		<li><a href="embeddable-chart.html" >Embeddable chart</a>
+			<ul>
+				<li><a href="metabolise/0.2/" >v0.2</a></li>
+			</ul>
+		</li>
+	</ul>
+</body>
+</html>
