e871132182c1d215a9c1431bcbd08ae75d27b27a
colchambers <colchambers@gmail.com>
2011-08-11 15:41:52 +0000

    added rpahael human to lifsetyle and added animate methods

--- 0.2/dev/lifestyle-metabolism/lifestyle.js
+++ 0.2/dev/lifestyle-metabolism/lifestyle.js
@@ -42,6 +42,7 @@ var Lifestyle = {
     requiredHours: 24,
     energyCorrect: false,
     hoursCorrect: false,
+    human: null,
 
     init: function() {
         
@@ -62,12 +63,26 @@ var Lifestyle = {
             this.calculateActivityEnergyDifferenceById(activityId);
         }
         
+        this.drawHuman();
+        
     },
     
     initialiseActivities: function() {
         this.activities[1] = new Activity(1, 'work', 0.5, 14);
         this.activities[2] = new Activity(2, 'rest', 0.5, 8);
         this.activities[3] = new Activity(3, 'play', -2, 2);
+    },
+    
+    /*
+             * @url http://raphaeljs.com/icons/
+             */
+    drawHuman: function () {
+        var paper = Raphael('human', 200, 200);//creates canvas width=height=100px
+        
+        this.human = paper.path("M21.021,16.349c-0.611-1.104-1.359-1.998-2.109-2.623c-0.875,0.641-1.941,1.031-3.103,1.031c-1.164,0-2.231-0.391-3.105-1.031c-0.75,0.625-1.498,1.519-2.111,2.623c-1.422,2.563-1.578,5.192-0.35,5.874c0.55,0.307,1.127,0.078,1.723-0.496c-0.105,0.582-0.166,1.213-0.166,1.873c0,2.932,1.139,5.307,2.543,5.307c0.846,0,1.265-0.865,1.466-2.189c0.201,1.324,0.62,2.189,1.463,2.189c1.406,0,2.545-2.375,2.545-5.307c0-0.66-0.061-1.291-0.168-1.873c0.598,0.574,1.174,0.803,1.725,0.496C22.602,21.541,22.443,18.912,21.021,16.349zM15.808,13.757c2.362,0,4.278-1.916,4.278-4.279s-1.916-4.279-4.278-4.279c-2.363,0-4.28,1.916-4.28,4.279S13.445,13.757,15.808,13.757z").attr({fill: "#000", stroke: "none"});
+        //human.animate({scale:"2"}, 2000);
+        //setTimeout(circle1.animate({scale:"1"}, 2000), 2000);
+    
     },
     
     getFieldValue: function(id){
@@ -226,6 +241,15 @@ var Lifestyle = {
     },
     
     /*
+     * Update the slider values and positions
+     * @return vaoid
+     */
+    updateHuman: function() {
+        
+        this.human.animate({scale:2+(this.totalEnergy/3)}, 2000);
+    },
+    
+    /*
      * @param int id activity id
      * @return vaoid
      */
@@ -237,6 +261,7 @@ var Lifestyle = {
         }
         this.displayTotals();
         this.updateSliders();
+        this.updateHuman();
     },
     
     /*
